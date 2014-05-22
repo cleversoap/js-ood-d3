1b88ff26df838397c9180d33ca8e4e1524a1431f
cleversoap <clever@cleversoap.com>
2014-05-22 12:30:37 +1000

    Manually removing the call to require js-ood with gulp-replace as the standalone is not detected and none of the options I've tried work.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
-var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
 gulp.task('browserify', function (callback) {
     gulp.src('lib/js-ood-d3.js')
     .pipe(browserify({
         external: 'js-ood',
+        ignore: 'js-ood',
+        exclude: 'js-ood',
         standalone: 'ood.d3'
     }))
+    .pipe(replace(/var ood = _dereq_.*$/gm, ''))
     .pipe(gulp.dest('./build'));
 
     gulp.src('lib/js-ood-d3.css')
